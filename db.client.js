622cb34c839c68cca6c128ebefc648660d398251
Yoploo <thierry.yoran@gmail.com>
2024-04-02 14:46:04 +0200

    db link

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DB_CONNECTION_STRING, // TODO
   {
     dialectOptions: {
       ssl: {
